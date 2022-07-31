6d44afd7e8474cd2fb468a1df146cbe334e675ea
shoxie <sktt1lka@gmail.com>
2022-07-31 15:30:43 +0700

    added seo options

---
+++ data/siteMeta.ts
@@ -0,0 +1,10 @@
+const siteMeta = {
+    siteUrl: "https://wrosedev.tech",
+    title: "WhiteRose Space",
+    description: "A place where WhiteRose gone mad with tech.",
+    author: "Đào Tuấn Anh",
+    email: "doctorwrose@gmail.com",
+    github: "https://github.com/shoxie"
+}
+
+export default siteMeta;
